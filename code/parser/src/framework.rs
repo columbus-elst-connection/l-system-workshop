cadefaa179177a77e499fd3c9eebd1da8e74ab72
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-09-21 09:39:17 +0200

    Introduce a framework for parser

---
+++ code/parser/src/framework.rs
@@ -0,0 +1,3 @@
+pub enum ParseError {
+    GenericError
+}
--- code/parser/src/lib.rs
+++ code/parser/src/lib.rs
@@ -1,7 +1,7 @@
+mod framework;
+
+use self::framework::ParseError;
+
 pub fn parse(_input: &str) -> Result<(), ParseError> {
   Err(ParseError::GenericError)
-}
-
-pub enum ParseError {
-    GenericError
 }
